e26d1abb27ca53f05bff5c72980758bd3b015def
tushar252003 <srmstushar@gmail.com>
2024-12-01 21:32:59 +0530

    Add files via upload

---
+++ st julis/script.js
@@ -0,0 +1,5 @@
+// Set up the sliding school name text
+const schoolName = "St. Martin Public School Bareilly";
+const schoolNameSlider = document.getElementById("school-name-slider");
+
+schoolNameSlider.innerText = schoolName;
